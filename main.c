5a32e918cc3199498e907b82b529b6057de2c849
Felipe Gil <fgil90@gmail.com>
2024-02-18 15:41:57 +0000

    Minor refactoring

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 //----------------------------------------------------------------------------------
 // TODO:
 // - [x] Camera
-// - [ ] Enemies
-// - [ ] Throw curved cards
+// - [x] Enemies
 // - [ ] Different Types of cards
 // - [ ] Cycle card types with Q and E
+// - [ ] Throw curved cards
 
 #include "raylib.h"
 #include "raymath.h"
@@ -25,6 +25,7 @@
 #define PLAYER_SIZE 64
 #define CARD_SIZE 16
 #define CARD_SPEED_MULT 1/25.0f
+#define SQRT2 1.41421356237
 
 #if defined(PLATFORM_WEB)
     #include <emscripten/emscripten.h>
@@ -36,10 +37,6 @@
 
 Color bgColor = WHITE;
 Vector2 playerSize = {(float)PLAYER_SIZE, (float)PLAYER_SIZE};
-
-// THIS WILL BECOME OBSOLETE AFTER I HAVE DIFFERENT TYPES OF CARDS
-Vector2 cardSize = {(float)CARD_SIZE, (float)CARD_SIZE}; 
-Color cardColor = RED;
 
 //----------------------------------------------------------------------------------
 // Player Definition
@@ -71,6 +68,7 @@ typedef struct Card{
     Vector2 center;
     Vector2 speed;
     Vector2 size;
+    Color color;
     float rotation;
     float rotationSpeed;
     bool isAlive;
@@ -103,6 +101,7 @@ typedef struct Enemy{
     Vector2 size;
     Color color;
     Vector2 pos;
+    Vector2 center;
     Vector2 curr_speed;
     float speed;
     bool isAlive;
@@ -223,23 +222,21 @@ void DrawPlayer(const PLAYER *p){
     DrawRectangleV(p->pos, playerSize, p->color);
 }
 
-void DrawCards(CARD_HANDLER *ch)
-{
+void DrawCards(CARD_HANDLER *ch){
     for (int i = 0; i < MAX_CARDS; i++){
-        if (!ch->cardArray[i].isAlive)
-            continue;
+        CARD *c = &ch->cardArray[i];
+        if (!c->isAlive) continue;
 
-        Rectangle rect = {ch->cardArray[i].pos.x,
-                          ch->cardArray[i].pos.y,
-                          cardSize.x,
-                          cardSize.y};
-        DrawRectanglePro(rect, ch->cardArray[i].center, ch->cardArray[i].rotation, cardColor);
+        Rectangle rect = {c->pos.x,
+                          c->pos.y,
+                          c->size.x,
+                          c->size.y};
+        DrawRectanglePro(rect, c->center, c->rotation, c->color);
     }
     return;
 }
 
-void UpdatePlayer(PLAYER *p)
-{
+void UpdatePlayer(PLAYER *p){
     // Move Player
     if (IsKeyDown(KEY_W))
         p->pos.y -= p->speed;
@@ -270,7 +267,7 @@ void MoveCard(CARD *c){
     c->pos.y += c->speed.y;
     c->speed.x *= 0.95f;
     c->speed.y *= 0.95f;
-    if (Vector2Equals(c->speed, (Vector2)ZERO_VECTOR)) c->isMoving = false;
+    if (Vector2DistanceSqr(c->speed, (Vector2)ZERO_VECTOR)<0.01f) c->isMoving = false;
 }
 
 void RotateCard(CARD *c){
@@ -280,8 +277,7 @@ void RotateCard(CARD *c){
 
 void HandleCardThrow(CARD_HANDLER *ch)
 {
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-    {
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
         ch->isShooting = true;
         ch->initialMousePos = GetMousePosition();
     }
@@ -289,11 +285,10 @@ void HandleCardThrow(CARD_HANDLER *ch)
     if (ch->isShooting)
         ch->shotTimeAccumulator += GetFrameTime();
 
-    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-    {
-        Vector2 cardSpeed = {CARD_SPEED_MULT * (GetMousePosition().x - ch->initialMousePos.x) / ch->shotTimeAccumulator,
-                             CARD_SPEED_MULT * (GetMousePosition().y - ch->initialMousePos.y) / ch->shotTimeAccumulator};
-        Vector2 playerCenter = {player.pos.x + player.center.x, player.pos.y + player.center.y};
+    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+        Vector2 cardDirection = Vector2Subtract(GetMousePosition(), ch->initialMousePos);
+        Vector2 cardSpeed = Vector2Scale(cardDirection, 1 / ch->shotTimeAccumulator * CARD_SPEED_MULT);
+        Vector2 playerCenter = Vector2Add(player.pos, player.center);
         ThrowCard(playerCenter, cardSpeed, ch);
         ch->shotTimeAccumulator = 0.0f;
         ch->isShooting = false;
@@ -303,21 +298,23 @@ void HandleCardThrow(CARD_HANDLER *ch)
 void ThrowCard(Vector2 origin, Vector2 speed, CARD_HANDLER *ch){
     ch->cardArray[ch->currentCardIdx] = (CARD){
         .pos = origin,
-        .center = (Vector2){cardSize.x / 2, cardSize.y / 2},
+        .center = (Vector2){CARD_SIZE / 2, CARD_SIZE / 2},
         .speed = speed,
-        .size = cardSize,
+        .size = (Vector2){CARD_SIZE, CARD_SIZE},
+        .color = RED,
         .rotation = (float)GetRandomValue(0, 360),
         .rotationSpeed = (float)GetRandomValue(30, 200),
         .isAlive = true,
-        .isMoving = true
-    };
+        .isMoving = true};
 
     ch->currentCardIdx += 1;
     ch->currentCardIdx %= MAX_CARDS;
 }
 
 void MoveEnemy(ENEMY *e, Vector2 target){
-    Vector2 direction = Vector2Subtract(Vector2Add(player.pos, player.center), e->pos);
+    Vector2 player_center = Vector2Add(player.pos, player.center);
+    Vector2 enemy_center = Vector2Add(e->pos, e->center);
+    Vector2 direction = Vector2Subtract(player_center, enemy_center);
     e->curr_speed = Vector2Scale(Vector2Normalize(direction), e->speed);
     e->pos = Vector2Add(e->pos, e->curr_speed);
 }
@@ -359,10 +356,11 @@ void CheckCardEnemyCollisions(CARD_HANDLER *ch, ENEMY_HANDLER *eh)
             if (!e->isAlive) continue;
 
             Rectangle enemy_rect = {e->pos.x, e->pos.y, e->size.x, e->size.y};
-            Rectangle  card_rect = {c->pos.x, c->pos.y, c->size.x, c->size.y};
-            if (CheckCollisionRecs(enemy_rect, card_rect)){
+            Vector2 cardCenter = Vector2Add(c->center, c->pos);
+            if (CheckCollisionCircleRec(cardCenter, c->size.x/2*SQRT2, enemy_rect)){
                 c->isAlive = false;
-                e->isAlive = false;                                         
+                e->isAlive = false;
+                return;
             }
         }
     }
@@ -372,6 +370,7 @@ void SpawnEnemy(ENEMY_HANDLER *eh){
     eh->enemyArray[eh->currentIndex] = (ENEMY){
         .pos = ZERO_VECTOR,
         .size = {30.0f, 30.0f},
+        .center = {15.0f, 15.0f},
         .speed = GetRandomValue(1, eh->difficulty*2),
         .hp = 3,
         .color = DARKPURPLE,
